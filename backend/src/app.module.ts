78b86dd1811cf1c8b705a5119d4779247562e19f
MedDarr <112845790+MedDarr@users.noreply.github.com>
2026-06-02 01:29:13 +0300

    Convert app module to NestJS module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,3 +1,5 @@
+import { Module } from '@nestjs/common';
+
 import { ApplicationsController } from './applications/applications.controller';
 import { ApplicationsService } from './applications/applications.service';
 import { SecurityChecksController } from './security-checks/security-checks.controller';
@@ -10,17 +12,21 @@ import { RoutingService } from './routing/routing.service';
 import { AuditController } from './audit/audit.controller';
 import { AuditService } from './audit/audit.service';
 
-export class AppModule {
-  readonly applicationsService = new ApplicationsService();
-  readonly securityChecksService = new SecurityChecksService();
-  readonly scoringService = new ScoringService();
-  readonly recommendationsService = new RecommendationsService();
-  readonly routingService = new RoutingService();
-  readonly auditService = new AuditService();
-
-  readonly applicationsController = new ApplicationsController(this.applicationsService);
-  readonly securityChecksController = new SecurityChecksController(this.securityChecksService);
-  readonly scoringController = new ScoringController(this.scoringService);
-  readonly recommendationsController = new RecommendationsController(this.recommendationsService);
-  readonly auditController = new AuditController(this.auditService);
-}
+@Module({
+  controllers: [
+    ApplicationsController,
+    SecurityChecksController,
+    ScoringController,
+    RecommendationsController,
+    AuditController,
+  ],
+  providers: [
+    ApplicationsService,
+    SecurityChecksService,
+    ScoringService,
+    RecommendationsService,
+    RoutingService,
+    AuditService,
+  ],
+})
+export class AppModule {}
